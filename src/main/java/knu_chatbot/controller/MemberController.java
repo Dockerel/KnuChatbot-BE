008a3b41dad2590ffea4aa230dad32c750df4a9f
Giheon Do <dgh0001@naver.com>
2025-01-29 13:07:35 +0900

    feat: signup path 추가

--- src/main/java/knu_chatbot/controller/MemberController.java
+++ src/main/java/knu_chatbot/controller/MemberController.java
@@ -1,6 +1,8 @@
 package knu_chatbot.controller;
 
+import jakarta.validation.Valid;
 import knu_chatbot.controller.request.MemberEmailCheckRequest;
+import knu_chatbot.controller.request.MemberSignupRequest;
 import knu_chatbot.controller.response.ApiResponse;
 import knu_chatbot.service.MemberService;
 import lombok.RequiredArgsConstructor;
@@ -17,8 +19,14 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/check-email")
-    public ApiResponse<Object> checkEmail(@RequestBody MemberEmailCheckRequest request) {
-        memberService.emailExists(request.getEmail());
+    public ApiResponse<Object> checkEmail(@Valid @RequestBody MemberEmailCheckRequest request) {
+        memberService.emailExists(request.toServiceRequest());
+        return ApiResponse.ok(null);
+    }
+
+    @PostMapping("/signup")
+    public ApiResponse<Object> signup(@Valid @RequestBody MemberSignupRequest request) {
+        memberService.signup(request.toServiceRequest());
         return ApiResponse.ok(null);
     }
 }
