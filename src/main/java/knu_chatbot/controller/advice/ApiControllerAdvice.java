88d8c8902cc0a97bb4511946bef2833b45b0363f
Giheon Do <dgh0001@naver.com>
2025-01-29 13:05:15 +0900

    feat: 바인딩 예외 처리 핸들러 추가

--- src/main/java/knu_chatbot/controller/advice/ApiControllerAdvice.java
+++ src/main/java/knu_chatbot/controller/advice/ApiControllerAdvice.java
@@ -2,6 +2,7 @@ package knu_chatbot.controller.advice;
 
 import knu_chatbot.controller.response.ApiResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,6 +16,16 @@ public class ApiControllerAdvice {
         return ApiResponse.of(
             HttpStatus.BAD_REQUEST,
             e.getMessage(),
+            null
+        );
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(BindException.class)
+    public ApiResponse<Object> bindException(BindException e) {
+        return ApiResponse.of(
+            HttpStatus.BAD_REQUEST,
+            e.getBindingResult().getAllErrors().get(0).getDefaultMessage(),
             null
         );
     }
