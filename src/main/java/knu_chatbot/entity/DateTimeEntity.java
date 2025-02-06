35a3d2d9127ccaf36f1b964a6bd73bf0e9b6d73c
Giheon Do <dgh0001@naver.com>
2025-02-06 22:02:46 +0900

    add: 테스트를 위해 setter 추가

--- src/main/java/knu_chatbot/entity/DateTimeEntity.java
+++ src/main/java/knu_chatbot/entity/DateTimeEntity.java
@@ -3,12 +3,14 @@ package knu_chatbot.entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter
+@Setter
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 public abstract class DateTimeEntity {
