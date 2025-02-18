2bf37c867482ababf2f50fa29d6535c2cfacf693
Giheon Do <dgh0001@naver.com>
2025-02-18 22:10:45 +0900

    feat: login check interceptor 추가

---
+++ src/main/java/knu_chatbot/config/WebConfig.java
@@ -0,0 +1,20 @@
+package knu_chatbot.config;
+
+import knu_chatbot.interceptor.LoginCheckInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor())
+            .order(1)
+            .addPathPatterns(
+                "/api/members/me",
+                "/api/histories/**"
+            );
+    }
+}
